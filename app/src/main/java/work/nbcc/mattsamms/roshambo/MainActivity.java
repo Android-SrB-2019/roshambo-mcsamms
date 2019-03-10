4c5e53432d3e410a3f47f5cfaec0398aefc95fce
Matthew Samms <mcsamms@gmail.com>
2019-03-10 19:25:37 -0300

    Initial Commit and Moved images and roshambo

---
+++ app/src/main/java/work/nbcc/mattsamms/roshambo/MainActivity.java
@@ -0,0 +1,13 @@
+package work.nbcc.mattsamms.roshambo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
